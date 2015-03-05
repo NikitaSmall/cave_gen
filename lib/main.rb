b879893d9fbc078efcecae0292ea5c1c874eadb2
nikitasmall <nikitasosnov@yahoo.com>
2015-03-05 21:18:11 +0200

    Created base map class

--- lib/main.rb
+++ lib/main.rb
@@ -2,4 +2,45 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-puts "Hello World"
+class Tile
+  attr_accessor :floor
+  attr_accessor :opacity
+
+  def initialize
+    @floor = 0;
+    @opacity = 0;
+  end
+
+end
+
+class Map
+
+  def initialize(height, width)
+    @height = height
+    @width = width
+
+    rows, cols = @width,@height
+    @grid = Array.new(rows) { Array.new(cols) }
+
+    @grid.each do |y|
+      y.each do |x|
+        x = Tile.new
+      end
+    end
+  end
+
+  def show_map
+    @grid.each do |y|
+      y.each do |x|
+        print "."
+      end
+      print "\n"
+    end
+  end
+
+end
+
+@map = Map.new(20, 20)
+
+@map.show_map
+
